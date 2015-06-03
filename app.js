5e4e8116f256158b5a8b9f69c083781b12b83c22
Andrew Ellis <astoellis@gmail.com>
2015-06-02 23:24:50 -0400

    Added a lot of front-end featurse, UI, and colors

--- app.js
+++ app.js
@@ -1,43 +1,73 @@
+var Scrape = Scrape || {};
+
 $(document).ready(function() {
-
-
-$('#moo').click(function(event) {
-  getCheddar();
-});
-
-
-
-
-  $( '#product-search' ).keypress(function(event) {
-    var checkStr = event.keyCode;
-    console.log(checkStr);
+  // $('body').addClass('hidden');
+  // $('body').fadeIn(1200).removeClass('hidden');
+$('jumbotron').addClass('fade-in');
+  $('button.form-control').on('click', function() {
+    console.log('clicked login');
+    var username = $('#uLogin').val();
+    var password = $('#uPassword').val();
     $.ajax({
-      url: 'http://localhost:9000/html-elements.json',
-      type: 'GET',
-      dataType: 'json',
-    })
-    .done(function(response) {
-      console.log("success");
-      // Set shit to null
-      $('#json-datalist').html('');
-
-      // Go through the json received and render the ones that match.
-      response.forEach(function (element) {
-        element = element.toString();
-        // if (element.match(/checkStr)) {}
-        var $newOption = '<option value="' + element + '"></option';
-
-        $('#json-datalist').append($newOption);
+        url: 'localhost:9000/login',
+        type: 'POST',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          credentials: {
+            email: $('#uLogin').val(),
+            password: $('#uPassword').val()
+          }
+        }),
+      })
+      .done(function(data, textStatus) {
+        console.log(textStatus);
+        console.log(data);
+        console.log('Success');
+      })
+      .fail(function(jqxhr, textStatus, errorThrown) {
+        console.log("error");
+        console.log(textStatus);
+      })
+      .always(function() {
+        console.log("complete");
       });
-    })
-    .fail(function() {
-      console.log("error");
-    })
-    .always(function() {
-      console.log("complete");
-    });
 
   });
+
+
+
+
+  // $( '#product-search' ).keypress(function(event) {
+  //   var checkStr = event.keyCode;
+  //   console.log(checkStr);
+  //   $.ajax({
+  //     url: 'http://localhost:9000/html-elements.json',
+  //     type: 'GET',
+  //     dataType: 'json',
+  //   })
+  //   .done(function(response) {
+  //     console.log("success");
+  //     // Set shit to null
+  //     $('#json-datalist').html('');
+
+  //     // Go through the json received and render the ones that match.
+  //     response.forEach(function (element) {
+  //       element = element.toString();
+  //       // if (element.match(/checkStr)) {}
+  //       var $newOption = '<option value="' + element + '"></option';
+
+  //       $('#json-datalist').append($newOption);
+  //     });
+  //   })
+  //   .fail(function() {
+  //     console.log("error");
+  //   })
+  //   .always(function() {
+  //     console.log("complete");
+  //   });
+
+  // });
 
 
 });
--- jankyscrape.js
+++ jankyscrape.js
@@ -1,19 +1,28 @@
-function getCheddar () {
-  $.ajax({
-    url:'http://www.benjerry.com/flavors',
-    type: 'GET',
-    dataType: 'html',
-  })
-  .done(function(response) {
-    console.log("success");
-    console.log(response);
-    console.log(response.html());
-  })
-  .fail(function() {
-    console.log("error");
-  })
-  .always(function() {
-    console.log("complete");
-  });
-}
+var Scrape = Scrape || {};
 
+Scrape.List = (function() {
+
+
+  function _getFlavors() {
+    $.ajax({
+        url: 'http://www.benjerry.com/flavors',
+        type: 'GET',
+        dataType: 'json',
+
+      })
+      .done(function() {
+        console.log("success");
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+  }
+
+  return {
+    getFlavors: _getFlavors
+  };
+
+})();
