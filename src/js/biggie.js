c4eb46fe12b8938f9912fdde22555c175fdc9b5a
Andrew Ellis <astoellis@gmail.com>
2015-06-08 19:52:57 -0400

    Added cute glyph icon

--- src/js/biggie.js
+++ src/js/biggie.js
@@ -3,12 +3,12 @@ var Scrape = Scrape || {};
 Scrape.New = (function() {
 
   var password, password_confirmation, email, firstName, lastName, user_token, user_id, foundPairings;
-loggedIn = false;
+  loggedIn = false;
 
 
 
 
-//NEEDS WORK
+  //NEEDS WORK
   function _hideLogin() {
     $('#loginModal').modal('hide');
     $('#login-button').hide();
@@ -16,7 +16,7 @@ loggedIn = false;
   }
 
 
-//NEEDS WORK
+  //NEEDS WORK
   function _success() {
     $('#login-success').modal('show');
     _hideLogin();
@@ -24,159 +24,153 @@ loggedIn = false;
     loggedIn = true;
   }
 
-//NEEDS WORK
+  //NEEDS WORK
   function _failure(failText) { //If login fails, hide modal and highlight erros
-    $('.alert-danger').html('<strong>'+failText+'</strong><br><p>Please check fields and try again</p>');
+    $('.alert-danger').html('<strong>' + failText + '</strong><br><p>Please check fields and try again</p>');
     $('#login-fail').modal('show');
   }
 
 
   /*CLEAR PASSWORD FIELDS AFTER SUCCESS*/
-  function _clearPass () {
-    $('#uEmail').val('');
-    $('#uEmailNew').val('');
-    $('#uPassword').val('');
-    $('#uPasswordNew').val('');
-    $('#firstName').val('');
-    $('#lastName').val('');
-    $('#uPasswordConfirmation').val('');
-  }
-  /*END CLEAR PASSWORD FIELDS AFTER SUCCESS*/
-
-
-/*CREDENTIALS GENERATOR*/
-  function _generateCredentials(userType) {
-    if (userType == "new") {
-      return JSON.stringify({
-        credentials: {
-          email: $('#uEmailNew').val(),
-          password: $('#uPasswordNew').val(),
-          first_name: $('#uFirstName').val(),
-          last_name: $('#uLastName').val(),
-          password_confirmation: $('#uPasswordConfirmation').val()
-        }
-      });
-    } else {
-      return JSON.stringify({
-        credentials: {
-          email: $('#uEmail').val(),
-          password: $('#uPassword').val()
-        }
-      });
+  function _clearPass() {
+      $('#uEmail').val('');
+      $('#uEmailNew').val('');
+      $('#uPassword').val('');
+      $('#uPasswordNew').val('');
+      $('#firstName').val('');
+      $('#lastName').val('');
+      $('#uPasswordConfirmation').val('');
     }
+    /*END CLEAR PASSWORD FIELDS AFTER SUCCESS*/
 
-  }
-/*END CREDENTIALS GENERATOR*/
 
-/*REGISTER FUNCTION*/
+  /*CREDENTIALS GENERATOR*/
+  function _generateCredentials(userType) {
+      if (userType == "new") {
+        return JSON.stringify({
+          credentials: {
+            email: $('#uEmailNew').val(),
+            password: $('#uPasswordNew').val(),
+            first_name: $('#uFirstName').val(),
+            last_name: $('#uLastName').val(),
+            password_confirmation: $('#uPasswordConfirmation').val()
+          }
+        });
+      } else {
+        return JSON.stringify({
+          credentials: {
+            email: $('#uEmail').val(),
+            password: $('#uPassword').val()
+          }
+        });
+      }
+
+    }
+    /*END CREDENTIALS GENERATOR*/
+
+  /*REGISTER FUNCTION*/
   function _register() {
-    params = _generateCredentials("new");
+      params = _generateCredentials("new");
+      $.ajax({
+          url: 'http://localhost:9000/register',
+          type: 'POST',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: params
+        })
+        .done(function() {
+          // console.log("success");
+          $('#loginModal').modal('hide');
+        })
+        .fail(function() {
+          _failure("Registration Error");
+          // console.log("error");
+        });
+    }
+    /*END REGISTER FUNCTION*/
+
+  /*LOGIN FUNCTION*/
+  function _login() {
+      params = _generateCredentials("old");
+      $.ajax({
+          url: 'http://localhost:9000/login',
+          type: 'POST',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: params
+        })
+        .done(function(data, textStatus) {
+          // console.log('Success');
+          user_token = data.token;
+          _success();
+          flavors = data.flavors;
+          drinks = data.drinks;
+        })
+        .fail(function(jqxhr, textStatus, errorThrown) {
+          // console.log("error");
+          _failure("Login Error");
+        });
+    }
+    /*END LOGIN FUNCTION*/
+
+  /*POPULATE PAIRINGS*/
+  function _popPairings(pairingList, objType) {
+    var searchType, dataType, gliphy, itemName, otherName, $typeName, thingName;
+      searchType = objType == "drink" ? "icecream-search" : "coffee-search";
+      dataType = objType == "drink" ? "flavor_name":"roast_name";
+      gliphy = objType == "drink" ? "glyphicon glyphicon-glass": "glyphicon glyphicon-ice-lolly";
+      thingName = objType == "drink" ? "roast_name" : "flavor_name";
+      $('#pairings-div').show();
+      $('#pairings-list').html('');
+      /*START FOR LOOP OF DOOM*/
+      for (var i = 0; i < pairingList.length; i++) {
+        itemName = pairingList[i][dataType];
+        otherName = pairingList[i][thingName];
+        $typeName = $('#'+searchType+' input').val();
+        if ($typeName == itemName) {
+          $('#pairings-list').append('<a href="#" class="list-group-item ' + gliphy + '">  | ' + otherName + '</a>');
+        }
+
+      }
+    }
+    /*END POPULATE PAIRINGS*/
+
+  /*GET PAIRINGS*/
+  function _getPairings(pairingSearch, objectType) {
+    params = _generateCredentials();
     $.ajax({
-        url: 'https://mysterious-ocean-5529.herokuapp.com/register',
-        type: 'POST',
+        url: 'http://localhost:9000/pairing',
+        type: 'GET',
+        headers: {
+          Authorization: "Token token=" + user_token
+        },
         contentType: 'application/json',
         dataType: 'json',
-        data: params
       })
-      .done(function() {
+      .done(function(data) {
         // console.log("success");
-        $('#loginModal').modal('hide');
+        foundPairings = data.pairing;
+        _popPairings(foundPairings, objectType);
       })
       .fail(function() {
-        _failure("Registration Error");
         // console.log("error");
-      });
-  }
-/*END REGISTER FUNCTION*/
-
-/*LOGIN FUNCTION*/
-  function _login() {
-    params = _generateCredentials("old");
-    $.ajax({
-        url: 'https://mysterious-ocean-5529.herokuapp.com/login',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: params
       })
-      .done(function(data, textStatus) {
-        // console.log('Success');
-        user_token = data.token;
-        _success();
-        flavors = data.flavors;
-        drinks = data.drinks;
-      })
-      .fail(function(jqxhr, textStatus, errorThrown) {
-        // console.log("error");
-        _failure("Login Error");
+      .always(function() {
+        // console.log("complete");
       });
-  }
-/*END LOGIN FUNCTION*/
-
-/*POPULATE PAIRINGS*/
-function _popPairings (pairingList, objType) {
-  var nameType = objType =="drink"? "roast_name":"name";
-  var IdType = objType == "drink"? "drink_id":"flavor_id";
-  var thingName;
-  $('#pairings-div').show();
-  $('#pairings-list').html('');
-
-/*START FOR LOOP OF DOOM*/
-  for (var i = 0; i < pairingList.length; i++) {
-    $.ajax({
-       url: 'https://mysterious-ocean-5529.herokuapp.com/'+objType+'/'+pairingList[i][IdType],
-       type: 'GET',
-       headers: {Authorization: "Token token="+user_token},
-       contentType: 'application/json',
-       dataType: 'json'
-     })
-     .done(function(data) {
-       thingName =  data[objType][nameType];
-       $('#pairings-list').append('<a href="#" class="list-group-item">' + thingName + '</a>');
-     });
 
   }
-}
-/*END POPULATE PAIRINGS*/
 
-function pairingNames (pairData) {
-
-}
-
-/*GET PAIRINGS*/
-function _getPairings (pairingSearch, objectType) {
-  params = _generateCredentials();
-  $.ajax({
-    url: 'https://mysterious-ocean-5529.herokuapp.com/pairing',
-    type: 'GET',
-    headers: {Authorization: "Token token="+user_token},
-    contentType: 'application/json',
-    dataType: 'json',
-  })
-  .done(function(data) {
-    // console.log("success");
-    foundPairings = data.pairing;
-    _popPairings(foundPairings, objectType);
-  })
-  .fail(function() {
-    // console.log("error");
-  })
-  .always(function() {
-    // console.log("complete");
-  });
-
-}
-
-/*END GET PAIRINGS*/
+  /*END GET PAIRINGS*/
 
 
-/*NORMALIZER*/
-  function normalize (textToNormalize) {
-  return textToNormalize.replace(/\W+|™|®/, '').toLowerCase();
+  /*NORMALIZER*/
+  function normalize(textToNormalize) {
+    return textToNormalize.replace(/\W+|™|®/, '').toLowerCase();
   }
 
 
-/*AUTOPOPULATOR*/
+  /*AUTOPOPULATOR*/
   function _autoPop(checkStr, $listEl, targetType) {
     // console.log(checkStr, $listEl, targetType);
     $listEl.html('');
@@ -210,28 +204,28 @@ function _getPairings (pairingSearch, objectType) {
   /*END AUTOPOPULATOR*/
 
   /*DESTROY USER*/
-// function _destroyUser () {
-//   $.ajax({
-//     url: 'mysterious-ocean-5529.herokuapp.com/users/'+user_id,
-//     type: 'DELETE',
-//     dataType: 'json',
-//     headers: {Authorization: "Token token="+user_token}
-//   })
-//   .done(function() {
-//     console.log("success");
-//     _failure('your account has been removed');
-//   })
-//   .fail(function() {
-//     console.log("error");
-//   })
-//   .always(function() {
-//     console.log("complete");
-//   });
+  // function _destroyUser () {
+  //   $.ajax({
+  //     url: 'mysterious-ocean-5529.herokuapp.com/users/'+user_id,
+  //     type: 'DELETE',
+  //     dataType: 'json',
+  //     headers: {Authorization: "Token token="+user_token}
+  //   })
+  //   .done(function() {
+  //     console.log("success");
+  //     _failure('your account has been removed');
+  //   })
+  //   .fail(function() {
+  //     console.log("error");
+  //   })
+  //   .always(function() {
+  //     console.log("complete");
+  //   });
 
-// }
+  // }
   /*END DESTROY USER*/
 
-/*RETURN FUNCTIONS*/
+  /*RETURN FUNCTIONS*/
   return {
     login: _login,
     register: _register,
@@ -239,6 +233,6 @@ function _getPairings (pairingSearch, objectType) {
     getPairings: _getPairings,
     fail: _failure
   };
-/*END RETURN FUNCTIONS*/
+  /*END RETURN FUNCTIONS*/
 
 })(); //IIFE END & INVOCATION
